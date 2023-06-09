6ecccde592f2c6553d4e11282a2c345823d9570e
wangxinxin <wangxinxin@sensetime.com>
2023-06-09 15:59:49 +0800

    fix

--- components/alert.tsx
+++ components/alert.tsx
@@ -14,25 +14,6 @@ const Alert = ({ preview }: Props) => {
         'bg-neutral-50 border-neutral-200': !preview,
       })}
     >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-            </>
-          )}
-        </div>
-      </Container>
     </div>
   )
 }
